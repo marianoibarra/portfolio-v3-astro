2b03f1995039ca990a126ba9f338b88ea8f5a5a4
Mariano Ibarra <marianoibarratesta@outlook.com>
2024-09-28 21:38:22 -0300

    Fix in Canonical URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
-import { defineConfig } from 'astro/config';
+import partytown from "@astrojs/partytown";
+import { defineConfig } from "astro/config";
+import { URL } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: "static",
+  site: URL,
+  integrations: [partytown()],
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
+    defaultLocale: "en",
+    locales: ["en", "es"],
     routing: {
-      prefixDefaultLocale: false
-    }
-  }
+      prefixDefaultLocale: false,
+    },
+  },
 });
--- src/config.ts
+++ src/config.ts
@@ -1,27 +1,24 @@
 const config = {
+  URL: "https://marianoibarra.com",
   SOCIAL_MEDIA: [
     {
-      name: 'GitHub',
-      icon: 'github',
-      url: 'https://github.com/marianoibarra',
+      name: "GitHub",
+      icon: "github",
+      url: "https://github.com/marianoibarra",
     },
     {
-      name: 'LinkedIn',
-      icon: 'linkedin',
-      url: 'https://www.linkedin.com/in/marianoibarra',
+      name: "LinkedIn",
+      icon: "linkedin",
+      url: "https://www.linkedin.com/in/marianoibarra",
     },
     {
-      name: 'Email',
-      icon: 'email',
-      url: 'marianoibarratesta@outlook.com',
-      copy: true
+      name: "Email",
+      icon: "email",
+      url: "marianoibarratesta@outlook.com",
+      copy: true,
     },
   ],
-  NAV_LINKS: [
-    '#about',
-    '#jobs',
-    '#projects'
-  ],
+  NAV_LINKS: ["#about", "#jobs", "#projects"],
   SKILLS: [
     "JavaScript (ES6+)",
     "TypeScript",
@@ -41,18 +38,10 @@ const config = {
   SECTION_HEIGHT_RATIO: 0.35,
   PROJECTS_LIMIT: 3,
   COLORS: {
-    green: '#64ffda',
-    navy: '#0f172a',
-    darkNavy: '#020c1b',
+    green: "#64ffda",
+    navy: "#0f172a",
+    darkNavy: "#020c1b",
   },
+};
 
-}
-  
-export const {
-  SOCIAL_MEDIA,
-  NAV_LINKS,
-  SKILLS,
-  SECTION_HEIGHT_RATIO,
-  PROJECTS_LIMIT,
-  COLORS,
-} = config
+export const { SOCIAL_MEDIA, NAV_LINKS, SKILLS, SECTION_HEIGHT_RATIO, PROJECTS_LIMIT, COLORS, URL } = config;
