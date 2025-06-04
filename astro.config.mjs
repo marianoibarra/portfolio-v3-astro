7f985b94905bae7e2bb612e8aa902bd8253dc141
Mariano Ibarra <marianoibarratesta@outlook.com>
2025-06-04 18:06:49 -0300

    feat: Mejoras en la carga de tipografias + fix accesibilidad en footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import partytown from "@astrojs/partytown";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import { URL } from "./src/config";
 
 // https://astro.build/config
@@ -7,6 +7,29 @@ export default defineConfig({
   output: "static",
   site: URL,
   integrations: [partytown()],
+  experimental: {
+    fonts: [
+      {
+        name: "Figtree",
+        cssVariable: "--font-figtree",
+        provider: fontProviders.google(),
+        // subsets: ["latin"],
+        styles: ["normal"],
+        weights: [400, 500, 600, 700],
+        fallbacks: ["sans-serif"],
+        unicodeRange: ["U+0020-007F", "U+00A0-00FF"],
+        display: "block",
+      },
+      {
+        name: "JetBrains Mono",
+        cssVariable: "--font-jetbrains",
+        provider: fontProviders.google(),
+        subsets: ["latin"],
+        weights: [400],
+        fallbacks: ["monospace"],
+      },
+    ],
+  },
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
